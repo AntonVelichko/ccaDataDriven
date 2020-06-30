e70879e8227bf36e6705fcbae38a219e763c356d
AntonVelio <aveliccko@gmail.com>
2020-06-29 22:10:05 -0700

    ss

--- test/elements.js
+++ test/elements.js
@@ -1,12 +1,14 @@
-describe('Complex Counter App', function () { //define suite title by passing a string
 
-    describe('Getting to the page', function () { //define sub-suite title by passing a string
+const general = require('./../data/selectors.json').general;
 
-        it('TC-001 Page title is Complex Counter App', function () { //define test title by passing a string
-            browser.url('https://likejean.github.io/homework-5/'); //open baseUrl
-            const title = browser.getTitle(); //get page title and assign it to the "title" variable
-            browser.pause(2000); //just pause to visually see that something is happening on the page
-            expect(title).toEqual('Complex Counter App'); //compare {title} (actual) and "Complex Counter App" (expected)
+describe('Complex Counter App', function () {
+
+    describe('Getting to the page', function () {
+
+        it('TC-001 Page title is Complex Counter App', function () {
+            browser.url('https://likejean.github.io/homework-5/');
+            const title = browser.getTitle();
+            expect(title).toEqual('Complex Counter App');
         })
 
     });
@@ -14,17 +16,18 @@ describe('Complex Counter App', function () { //define suite title by passing a 
     describe('Elements exist', function () {
 
         it('TC-002 Header', function () {
-            const header = $('h1').isDisplayed();
+            const header = general.header;
+            const actual = header.waitForDisplayed();
+            expect(actual).toEqual(true);
+        })
+
+        xit('TC-003 Total Result', function () {
+            const header = $('h3.total-count').waitForDisplayed();
             expect(header).toEqual(true);
         })
 
-        it('TC-003 Total Result', function () {
-            const header = $('h3.total-count').isDisplayed();
-            expect(header).toEqual(true);
-        })
-
-        it('TC-004 Counter Name', function () {
-            const header = $$('h3')[1].isDisplayed();
+        xit('TC-004 Counter Name', function () {
+            const header = $$('h3')[1].waitForDisplayed();
             expect(header).toEqual(true);
         })
 
